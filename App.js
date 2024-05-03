58865d12c262af46b5ee46cdb5089d71c32dd6a8
Dileep <dileep.thummaluru@gmail.com>
2024-05-03 12:00:51 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 element"),
+    React.createElement("h2", {}, "I'm an h2 element"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 element"),
+    React.createElement("h2", {}, "I'm an h2 element"),
+  ]),
+]);
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
